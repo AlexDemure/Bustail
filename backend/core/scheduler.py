39c7c09c2ecbb23d4ea7ac91308e9ed64cd3af2e
AlexDemure <alexanderdemure@gmail.com>
2021-02-09 14:58:28 +0500

    asp

--- backend/core/scheduler.py
+++ backend/core/scheduler.py
@@ -1,10 +1,9 @@
-# from apscheduler.schedulers.asyncio import AsyncIOScheduler
-#
-# from apps.projects.logic import upload_rss_everyday
-# from utils.overrides import get_actual_timezone
-#
-#
-# def start():
-#     sched = AsyncIOScheduler(timezone=get_actual_timezone())
-#     sched.add_job(upload_rss_everyday, 'cron', hour=10)
-#     sched.start()
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from backend.apps.applications.tasks import completed_applications
+
+
+def start():
+    scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
+    scheduler.add_job(completed_applications, 'cron', hour=8)
+    scheduler.start()
