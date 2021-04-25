6537e38d87e249caabc794b4740dafc2f62abcb5
AlexDemure <alexanderdemure@gmail.com>
2021-04-25 13:24:03 +0500

    payment

--- frontend/src/pages/cabinet/components/payment_data.js
+++ frontend/src/pages/cabinet/components/payment_data.js
@@ -1,32 +1,55 @@
+import React from 'react'
+
+import sendRequest from '../../../utils/fetch'
+
 import './css/payment_data.css'
 
-function PaymentData(props) {
-    return (
-        <div className="cabinet__driver__payment-data">
-            <div className="cabinet__driver__payment-data__card-design">
-                <p className="cabinet__driver__payment-data__item__text">Bustail</p>
+export default class PaymentData extends React.Component {
+    
+    constructor(props) {
+        super(props)
+    }
+
+    getPaymentLink = () => {
+        sendRequest("/api/v1/payments/", "GET")
+        .then(
+            (result) => {
+                window.location.replace(result.payment_url)
+            },
+            (error) => {
+                console.log(error.message)
+            }
+        )    
+    }
+
+    render() {
+        return (
+            <div className="cabinet__driver__payment-data">
+                <div className="cabinet__driver__payment-data__card-design">
+                    <p className="cabinet__driver__payment-data__item__text">Bustail</p>
+                </div>
+                <div className="cabinet__driver__payment-data__total-amount">
+                    <p className="cabinet__driver__payment-data__item__number">{this.props.total}</p>
+                    <p className="cabinet__driver__payment-data__item__text">Заработано</p>
+                </div>
+                <div className="cabinet__driver__payment-data__pay-btn" onClick={this.getPaymentLink}>
+                    <p>Оплатить</p>
+                </div>
+                <div className="cabinet__driver__payment-data__debt-amount">
+                    <p className="cabinet__driver__payment-data__item__number">{this.props.debt}</p>
+                    <p className="cabinet__driver__payment-data__item__text">Долг</p>
+                </div>
+                <div className="cabinet__driver__payment-data__commission-amount">
+                    <p className="cabinet__driver__payment-data__item__number">{this.props.commission}</p>
+                    <p className="cabinet__driver__payment-data__item__text">Коммиссия</p>
+                </div>
+                <div className="cabinet__driver__payment-data__limit-amount">
+                    <p className="cabinet__driver__payment-data__item__number">{this.props.limit}</p>
+                    <p  className="cabinet__driver__payment-data__item__text">Лимит</p>
+                </div>
             </div>
-            <div className="cabinet__driver__payment-data__total-amount">
-                <p className="cabinet__driver__payment-data__item__number">{props.total}</p>
-                <p className="cabinet__driver__payment-data__item__text">Заработано</p>
-            </div>
-            <div className="cabinet__driver__payment-data__pay-btn">
-                <p>Оплатить</p>
-            </div>
-            <div className="cabinet__driver__payment-data__debt-amount">
-                <p className="cabinet__driver__payment-data__item__number">{props.debt}</p>
-                <p className="cabinet__driver__payment-data__item__text">Долг</p>
-            </div>
-            <div className="cabinet__driver__payment-data__commission-amount">
-                <p className="cabinet__driver__payment-data__item__number">{props.commission}</p>
-                <p className="cabinet__driver__payment-data__item__text">Коммиссия</p>
-            </div>
-            <div className="cabinet__driver__payment-data__limit-amount">
-                <p className="cabinet__driver__payment-data__item__number">{props.limit}</p>
-                <p  className="cabinet__driver__payment-data__item__text">Лимит</p>
-            </div>
-        </div>
-    )
+        )
+    }
+    
 }
 
-export default PaymentData
