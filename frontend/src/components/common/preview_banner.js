a656ed422a377b74b1134be67149ee25f2be90f4
Пользователь <alexanderdemure@gmail.com>
2021-04-26 18:17:22 +0500

    h1

--- frontend/src/components/common/preview_banner.js
+++ frontend/src/components/common/preview_banner.js
@@ -4,7 +4,7 @@ import './css/preview_banner.css'
 function PreviewBanner() {
     return (
         <div className="preview-banner__common" onClick={() => window.location.replace('/')}>
-            <h1 className="preview-banner__common__title">bustail</h1>
+            <h1 className="preview-banner__common__title">Bustail</h1>
             <h2 className="preview-banner__common__description">сервис пассажирских перевозок</h2>
         </div>
     )
