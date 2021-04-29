c3fa3a684ed579b9c45bd94187c066a214197b53
AlexDemure <alexanderdemure@gmail.com>
2021-04-29 14:16:04 +0500

    notify

--- frontend/src/components/common/response_notify.js
+++ frontend/src/components/common/response_notify.js
@@ -6,34 +6,23 @@ import './css/response_notify.css'
 export function showNotify() {
     let element = document.getElementsByClassName("response-notify__common__border")[0]
     
-    if (element.style.opacity !== "0") {
-        setTimeout(() => {
-            element.style.opacity = "0"
-        }, 2000);
-        setTimeout(() => {
-            if (element.style.zIndex !== "-1") {
-                element.style.zIndex = "-1"
-            }
-        }, 3000);
-    }
-
     element.style.opacity = "1"
     element.style.zIndex = "50000"
     
+    setTimeout(() => {
+        element.style.opacity = "0"
+    }, 2000);
+    setTimeout(() => {
+        element.style.zIndex = "-1"
+    }, 3000);
 }
 
 export function closeNotify() {
     let element = document.getElementsByClassName("response-notify__common__border")[0]
-    
-    if (element.style.opacity !== "0") {
-        element.style.opacity = "0"
-        setTimeout(() => {
-            if (element.style.zIndex !== "-1") {
-                element.style.zIndex = "-1"
-            }
-        }, 1000);
-    }
-    
+    element.style.opacity = "0"
+    setTimeout(() => {
+        element.style.zIndex = "-1"
+    }, 1000);
 }
 
 export function ResponseNotify(props) {
