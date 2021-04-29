51f7204ace48aa43456bfa957cec6a94c9e28346
AlexDemure <alexanderdemure@gmail.com>
2021-04-29 11:49:14 +0500

    offer

--- frontend/src/components/common/buttons/submit_btn.js
+++ frontend/src/components/common/buttons/submit_btn.js
@@ -2,7 +2,7 @@ import './css/base.css'
 
 function SubmitButton(props) {
     return (
-        <input
+        <button
         type="submit"
         className={"input__common__submit " + (props.className ? props.className : "")}
         value={props.value ? props.value : "Отправить"}
