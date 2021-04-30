5b6856fd2d53f9af0c5a1745e0a3e9bdcd723791
AlexDemure <alexanderdemure@gmail.com>
2021-04-30 13:10:26 +0500

    history

--- frontend/src/pages/main/index.js
+++ frontend/src/pages/main/index.js
@@ -22,32 +22,36 @@ export default class MainPage extends React.Component {
     }
     render() {
         return (
-            <div className="container main">
+            <React.Fragment>
                 <Header page_name="Bustail"/>
-                <div className="main__menu">
-                    <div onClick={() => this.redirect('/app/create')} className="main__menu__btn__create-app">
-                        <p>Создать заявку</p>
-                    </div>
-                    <div onClick={() => this.redirect('/transport/create')} className="main__menu__btn__create-transport">
-                        <p>Предложить аренду</p>
-                    </div>
-                    <div className="main__menu__btn__find">
-                        <div id="text">
-                            <p>Поиск</p>
+
+                <div className="container main">
+                    <div className="main__menu">
+                        <div onClick={() => this.redirect('/app/create')} className="main__menu__btn__create-app">
+                            <p>Создать заявку</p>
                         </div>
-                        <div onClick={() => this.redirect('/transport/search')} id="transport">
-                            <p>Транспорт</p>
+                        <div onClick={() => this.redirect('/transport/create')} className="main__menu__btn__create-transport">
+                            <p>Предложить аренду</p>
                         </div>
-                        <div onClick={() => this.redirect('/app/search')} id="apps">
-                            <p>Заказы</p>
+                        <div className="main__menu__btn__find">
+                            <div id="text">
+                                <p>Поиск</p>
+                            </div>
+                            <div onClick={() => this.redirect('/transport/search')} id="transport">
+                                <p>Транспорт</p>
+                            </div>
+                            <div onClick={() => this.redirect('/app/search')} id="apps">
+                                <p>Заказы</p>
+                            </div>
                         </div>
-                    </div>
-                    <div onClick={() => this.redirect('/cabinet')} className="main__menu__btn__cabinet">
-                        <p>Личный кабинет</p>
+                        <div onClick={() => this.redirect('/cabinet')} className="main__menu__btn__cabinet">
+                            <p>Личный кабинет</p>
+                        </div>
                     </div>
                 </div>
+                
                 <NavBar/>
-            </div>
+            </React.Fragment>
         )
     }
     
