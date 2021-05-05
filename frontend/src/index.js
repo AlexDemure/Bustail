f39936416e8a222fc78c5e49ffb95c2d1bbfe4c9
AlexDemure <alexanderdemure@gmail.com>
2021-05-05 23:18:26 +0500

    try pwa

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,7 @@ import CreateAppPage from './pages/app/create'
 
 import './index.css'
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <Router>
@@ -49,6 +50,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // #181818 ФОН
 // #222222 ДИВ + БАТТОНС
