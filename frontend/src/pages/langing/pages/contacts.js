6a21a94219316899e0765c5b5d2402240a4ee9db
Пользователь <alexanderdemure@gmail.com>
2021-05-13 13:52:40 +0500

    docs

--- frontend/src/pages/langing/pages/contacts.js
+++ frontend/src/pages/langing/pages/contacts.js
@@ -4,13 +4,17 @@ import ContactsFeedBackForm from '../forms/form_contacts'
 
 import './css/base.css'
 import './css/contacts.css'
+import { Link } from 'react-router-dom'
 
 
 function ContactsPage(props) {
     return (
         <div className="contacts page">
             <div className="footer">
+                <a href="/docs/privecy.docx" download className="docs">Политика конфиденциальности</a>
+                <a href="/docs/terms.docx" download className="docs">Пользовательское соглашение</a>
                 <div id="preview-btn" className="anchor" onClick={props.changeForm}></div>
+                
             </div>
             <ContactsFeedBackForm/>
         </div>
