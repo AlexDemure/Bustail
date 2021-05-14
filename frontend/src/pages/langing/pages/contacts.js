6eb88f2df7e3cf7d0f87cbc071f9fe2f1b3ac743
Пользователь <alexanderdemure@gmail.com>
2021-05-14 10:12:04 +0500

    fix docs + fix contacts

--- frontend/src/pages/langing/pages/contacts.js
+++ frontend/src/pages/langing/pages/contacts.js
@@ -4,17 +4,12 @@ import ContactsFeedBackForm from '../forms/form_contacts'
 
 import './css/base.css'
 import './css/contacts.css'
-import { Link } from 'react-router-dom'
-
 
 function ContactsPage(props) {
     return (
         <div className="contacts page">
             <div className="footer">
-                <Link to="/docs/privecy.docx" download target="_blank" className="docs">Политика конфиденциальности</Link>
-                <Link to="/docs/terms.docx" download target="_blank" className="docs">Пользовательское соглашение</Link>
                 <div id="preview-btn" className="anchor" onClick={props.changeForm}></div>
-                
             </div>
             <ContactsFeedBackForm/>
         </div>
