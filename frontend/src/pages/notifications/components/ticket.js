f0cb009e7ec8d93f8499970336dc153612361231
AlexDemure <alexanderdemure@gmail.com>
2021-05-19 22:33:14 +0500

    try fix

--- frontend/src/pages/notifications/components/ticket.js
+++ frontend/src/pages/notifications/components/ticket.js
@@ -22,10 +22,9 @@ export default class TicketNotification extends React.Component {
                         <div className="ticket__notification__circle bottom"></div>
                     </div>
                     
-                    <p id='from' className="ticket__notification__city from">{this.props.ticket.to_go_from}</p>
-                    <p id="to" className="ticket__notification__city to">{this.props.ticket.to_go_to}</p>
-                
-                    
+                    <p id='from' className="ticket__notification__city from"><span>от</span> {this.props.ticket.to_go_from}</p>
+                    <p id="to" className="ticket__notification__city to"><span>куда</span> {this.props.ticket.to_go_to}</p>
+
                     <p className="ticket__notification__date">{new_date}</p>
                     <p className="ticket__notification__price">
                         <span>{`Цена: ${isChanged ? '(изм) ' : ""}`}</span>{price}
