ec673534a2a3a800b1df40c771e24ad2d36fb5f0
Пользователь <alexanderdemure@gmail.com>
2021-05-20 18:13:06 +0500

    check inn update

--- backend/api/routers/company.py
+++ backend/api/routers/company.py
@@ -163,6 +163,14 @@ async def change_company_data(
             detail=BaseMessage.obj_is_not_found.value
         )
 
+     # Проверка ИНН по базе
+    _, is_find = await DaCompany().find(payload.inn)
+    if not is_find:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=CompanyErrors.inn_not_found.value
+        )
+
     try:
         company = await logic_change_company_data(company, payload)
     except ValueError as e:
