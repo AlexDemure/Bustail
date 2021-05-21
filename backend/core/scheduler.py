4930d5b127fcf201126afd3694b9b408860a6850
AlexDemure <alexanderdemure@gmail.com>
2021-05-21 14:11:36 +0500

    expire application

--- backend/core/scheduler.py
+++ backend/core/scheduler.py
@@ -1,10 +1,11 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from backend.apps.applications.tasks import completed_applications, in_progress_applications
+from backend.apps.applications.tasks import completed_applications, in_progress_applications, expire_applications
 
 
 def start():
     scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
     scheduler.add_job(in_progress_applications, 'cron', hour=6)
     scheduler.add_job(completed_applications, 'cron', hour=8)
+    scheduler.add_job(expire_applications, 'cron', hour=10)
     scheduler.start()
