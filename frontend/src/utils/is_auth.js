72e297606ca65cc0205df28bf5ef94039e397366
Пользователь <alexanderdemure@gmail.com>
2021-05-21 08:02:27 +0500

    fixs

--- frontend/src/utils/is_auth.js
+++ frontend/src/utils/is_auth.js
@@ -1,6 +1,6 @@
 export default function isAuth(redirect = false) {
     let token = localStorage.getItem("token")
-    let is_confirmed = localStorage.get("is_confirmed")
+    let is_confirmed = localStorage.getItem("is_confirmed")
 
     if (token === null || (is_confirmed === false || is_confirmed === null) ) {
         if (!redirect) {
