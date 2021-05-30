d9532b6f722e50a052fd2afd37ac5401033823d8
AlexDemure <alexanderdemure@gmail.com>
2021-05-30 17:12:53 +0500

    revert img

--- backend/submodules/object_storage/utils.py
+++ backend/submodules/object_storage/utils.py
@@ -86,9 +86,7 @@ def compression_image(file: IO, content_type: FileMimetypes) -> bytes:
     """
     with tempfile.NamedTemporaryFile() as temp_file:
         image = Image.open(file)
-        if image.width / image.height <= 1:
-            image = image.rotate(-90)
-        image.thumbnail([1024, 768], Image.ANTIALIAS)
+        image = image.resize((1024, 768), Image.ANTIALIAS)
         image.save(temp_file, content_type.pillow_format, quality=85, optimize=True)
         temp_file.seek(0)
         return temp_file.file.read()
