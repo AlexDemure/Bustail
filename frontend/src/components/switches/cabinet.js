9a11fc7c8a3581c38c8b93b29285b2be0a88eba3
AlexDemure <alexanderdemure@gmail.com>
2021-06-12 11:28:15 +0500

    cursor + transport width

--- frontend/src/components/switches/cabinet.js
+++ frontend/src/components/switches/cabinet.js
@@ -7,7 +7,7 @@ function CabinetSwitch(props) {
         <div className="switch inline cabinet">
             <p className={`switch__item ${props.is_active === 'common' ? "active" : "no-active"}`} id="common" onClick={props.onClick}>Общее</p>
             <p className={`switch__item ${props.is_active === 'client' ? "active" : "no-active"}`} id="client" onClick={props.onClick}>Клиент</p>
-            <p className={`switch__item ${props.is_active === 'driver' ? "active" : "no-active"}`} id="driver" onClick={props.onClick}>Перевозчик</p>
+            <p className={`switch__item ${props.is_active === 'driver' ? "active" : "no-active"}`} id="driver" onClick={props.onClick}>Частник</p>
             <p className={`switch__item ${props.is_active === 'company' ? "active" : "no-active"}`} id="company" onClick={props.onClick}>Компания</p>
         </div>
     )
